da75efb4eae637a9f80b196d8e1f7ee10e036cb5
Surya Teja <suryateja2neti@gmail.com>
2024-11-27 09:52:25 +0530

    small change

--- backend/models/logos.js
+++
@@ -1,9 +0,0 @@
-const mongoose=require('mongoose')
-const Schema = mongoose.Schema
-const logoschema = new Schema({
-    teamname:String,
-    avatar:String
-})
-const Logo = mongoose.model('logos',logoschema)
-module.exports=Logo
-  
