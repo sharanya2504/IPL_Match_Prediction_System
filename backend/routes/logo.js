9c43fd52068cacf8c218cb74c1adb25f2653ce51
Surya Teja <suryateja2neti@gmail.com>
2024-11-27 10:41:08 +0530

    changed folders

--- backend/routes/logo.js
+++
@@ -1,7 +0,0 @@
-const express = require("express")
-const logocontroller = require('../controllers/logocontroller')
-const upload= require('../middleware/logos')
-const router = express.Router()
-router.post('/store',upload.single('avatar'),logocontroller.store)
-router.post('/findlogo',logocontroller.retrieve)
-module.exports=router   
